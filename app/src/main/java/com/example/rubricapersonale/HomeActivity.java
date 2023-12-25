5b48f50facbf8dfb00aa2d57924f7edf2adce422
Stefano <stebakite@gmail.com>
2023-12-25 10:15:41 +0100

    alcuni aggiustamenti  emersi nel test di call activity dal menù

--- app/src/main/java/com/example/rubricapersonale/HomeActivity.java
+++ app/src/main/java/com/example/rubricapersonale/HomeActivity.java
@@ -13,6 +13,8 @@ public class HomeActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        esci = findViewById(R.id.bExit);
 
         // implemento i listener sui bottoni
         esci.setOnClickListener(new View.OnClickListener() {
